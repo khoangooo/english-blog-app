a3abe767f58bc65531ceb88a5912b325857a5e0d
khoangooo <ngokhoa9596@gmail.com>
2021-08-29 20:40:25 +0700

    update

--- src/pages/editor/cover/index.js
+++ src/pages/editor/cover/index.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import imagesApi from "../../../api/apiImages";
-import Base64 from "../../../utils";
+import { CircularProgress, Typography } from "@material-ui/core";
 
 function EditorComponent(props) {
   const { detail, setDetail, title, setTitle } = props;
 
   const [file, setFile] = useState(null);
-  const [imagePreview, setImagePreview] = useState(null);
+  const [imageURL, setImageURL] = useState("");
+  const [uploadingImg, setUploadingImg] = useState(false);
+  const [currentImageId, setCurrentImageIg] = useState("");
 
   useEffect(() => {
     (async function () {
@@ -14,52 +16,65 @@ function EditorComponent(props) {
         let reader = new FileReader();
         reader.readAsDataURL(file);
         reader.onloadend = async function () {
-          console.log("file.name", file.name)
-          const res =await imagesApi.uploadImages({file: reader.result, fileName: file.name})
-          console.log("res", res)
+          setUploadingImg(true);
+          const res = await imagesApi.uploadImages({ file: reader.result, fileName: file.name });
+          if (res.status) {
+            setUploadingImg(false);
+            // setImageURL(URL.createObjectURL(file));
+            setImageURL(res.data.image_url)
+          } else {
+            setUploadingImg(false);
+            setImageURL("");
+          }
         };
         reader.onerror = function (error) {
+          setUploadingImg(false);
           console.log("Error: ", error);
         };
-        
       }
     })();
   }, [file]);
 
   const handleChangeFile = (e) => {
     setFile(e.target.files[0]);
-    setImagePreview(URL.createObjectURL(e.target.files[0]));
   };
 
   return (
     <div style={{ margin: "32px 64px" }}>
       <div style={{ display: "flex", alignItems: "center" }}>
-        {file !== null && (
+        {imageURL && (
           <img
             id="coverImageUploaded"
-            src={imagePreview}
+            src={imageURL}
             className="cover-img-uploaded"
             width={250}
             height={105}
             alt="Post cover"
           />
         )}
-        <div className="cover-image-uploaded-wrapper-btn">
-          <button className={file === null ? "cover-img__btn" : "cover-img__btn--change"}>
-            <label htmlFor="upload-cover" className="cover-img-label">
-              {file === null ? "Add a cover image" : "Change"}
-            </label>
-            <input
-              id="upload-cover"
-              type="file"
-              className="cover-img-input"
-              accept="image/*"
-              onChange={handleChangeFile}
-            />
-          </button>
-          {/* <button className="cover-img__btn--change">Change</button> */}
-          {file !== null && <button className="cover-img__btn--remove">Remove</button>}
-        </div>
+        {uploadingImg ? (
+          <>
+            <CircularProgress size={30}/>
+            <Typography variant="h6" component="h6">Uploading...</Typography>
+          </>
+        ) : (
+          <div className="cover-image-uploaded-wrapper-btn">
+            <button className={file === null ? "cover-img__btn" : "cover-img__btn--change"}>
+              <label htmlFor="upload-cover" className="cover-img-label">
+                {file === null ? "Add a cover image" : "Change"}
+              </label>
+              <input
+                id="upload-cover"
+                type="file"
+                className="cover-img-input"
+                accept="image/*"
+                onChange={handleChangeFile}
+              />
+            </button>
+            {/* <button className="cover-img__btn--change">Change</button> */}
+            {file !== null && <button className="cover-img__btn--remove">Remove</button>}
+          </div>
+        )}
       </div>
     </div>
   );
